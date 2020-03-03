14bf000d95a328f1fde0808d2dfa5dde85f71848
Tom Bacon <47856416+Tom147@users.noreply.github.com>
2020-03-03 09:38:46 +0100

    Add files via upload

---
+++ php/commentaire.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../css/commentaire.css">
+	<link rel="shortcut icon" href="../img/favicon.ico" />
+	<title>Commentaire</title>
+</head>
+<body>
+	<nav>
+		<div class="logo">
+			<a href="../index.php" id="left"><img src="../img/logoFinal.png" id="logo"></a>
+		</div>
+	</nav>
+
+<?php
+
+$bdd = new PDO('mysql:host=pj61y.myd.infomaniak.com;dbname=pj61y_espace_commentaires;charset=utf8','pj61y_tb14w',''); //connexion bdd
+
+if (isset($_GET['id']) AND !empty($_GET['id'])) {
+	$getid = htmlspecialchars($_GET['id']); 
+
+	$projet = $bdd->prepare('SELECT * FROM projets WHERE id = ?');	//prepare la requete
+	$projet->execute(array($getid));								//execute
+	$projet = $projet->fetch();										//vient chercher
+
+	$avis = $bdd->prepare('SELECT * FROM avis WHERE id_projet = ?');
+	$avis->execute(array($getid));
+
+	if (isset($_POST['submit_commentaire'])) {
+		if (isset($_POST['nom'],$_POST['commentaire']) AND !empty($_POST['nom']) AND !empty($_POST['commentaire'])) {
+			$nom = htmlspecialchars($_POST['nom']);
+			$commentaire = htmlspecialchars($_POST['commentaire']);
+
+			if(strlen($nom) < 25) {
+				$ins = $bdd->prepare('INSERT INTO avis (nom, commentaire, id_projet) VALUES (?,?,?)');
+				$ins->execute(array($nom, $commentaire, $getid));
+				$c_msg = "le commentaire a bien était pris en compte";
+
+			}else {
+				$c_msg = "Le nom doit faire moins de 25 caractères.";
+			}
+			
+		}else {
+			$c_msg = "Tous les champs doivent être complétés.";
+		}
+		
+	}
+
+	$avis = $bdd->prepare('SELECT * FROM avis WHERE id_projet = ? ORDER BY id DESC');
+	$avis->execute(array($getid));
+
+?>
+<section>
+	<div class="com">
+	<h2>Projet: <?= $projet['contenu'] ?></h2>
+
+	<br />
+
+	<h2>avis :</h2>
+
+	<form method="POST" >
+		<input type="text" name="nom" placeholder="Votre nom"><br/>
+		<textarea name="commentaire" placeholder="votre commentaire..."></textarea><br/>
+		<input type="submit" name="submit_commentaire" value="Envoyer" id="send">
+	</form>
+
+	<?php if(isset($c_msg)) { echo $c_msg; } ?>
+
+	<br>
+
+	<?php while($c = $avis->fetch()) { ?>
+		<b><?= $c['nom'] ?>:</b> <?= $c['commentaire'] ?><br/>
+	<?php } ?>
+
+	<?php
+	}
+	?>
+	</div>
+</section>
+	
+</body>
+</html>
+
+
